281cc5a20c04d64cf9d370b3c337fe3f43b00769
Gantyuk <Vgantyuk2@gmail.com>
2018-08-03 20:54:12 +0300

    Multiple Authentication

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,19 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/users/logout', 'Auth\LoginController@userLogout')->name('user.logout');
+
+
 Route::get('/home', 'HomeController@index')->name('home');
+Route::prefix('admin')->group(function () {
+    Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+    Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+    Route::get('/', 'AdminController@index')->name('admin');
+    Route::get('/logout', 'Auth\AdminLoginController@logout')->name('admin.logout');
+
+    // Password reset routes
+    Route::post('/password/email', 'Auth\AdminForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
+    Route::get('/password/reset', 'Auth\AdminForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
+    Route::post('/password/reset', 'Auth\AdminResetPasswordController@reset');
+    Route::get('/password/reset/{token}', 'Auth\AdminResetPasswordController@showResetForm')->name('admin.password.reset');
+});
